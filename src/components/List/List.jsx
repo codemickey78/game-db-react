3d0f83c5defbe0cdf57b72c3d120cc9d577f5b27
codemickey78 <codemickey78@gmail.com>
2022-05-25 17:35:54 +0000

    added search feature to list

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -2,13 +2,15 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import GameCard from "../GameCard/GameCard";
+import Search from "../Search/Search";
 
 const List = () => {
   const [games, setGames] = useState([]);
   const [nextPage, setNextPage] = useState("");
+  const [dataFromChild, setDataFromChild] = useState("");
 
-  const getGames = (reqType) => {
-    if (reqType === "first") {
+  const getGames = (reqType, searchTerm) => {
+    if (reqType === "first" && searchTerm === "") {
       axios
         .get(
           `https://api.rawg.io/api/games?key=${process.env.REACT_APP_RAWG_API_KEY}`
@@ -17,8 +19,18 @@ const List = () => {
           setGames(res.data.results);
           setNextPage(res.data.next);
         });
+    } else if (reqType === "search" && searchTerm !== "") {
+      axios
+        .get(
+          `https://api.rawg.io/api/games?key=${process.env.REACT_APP_RAWG_API_KEY}&search='${searchTerm}'`
+        )
+        .then((res) => {
+          setGames(res.data.results);
+          setNextPage(res.data.next);
+        });
     } else {
       axios.get(nextPage).then((res) => {
+        console.log(res.data.results);
         setGames(games.concat(res.data.results));
         setNextPage(res.data.next);
       });
@@ -29,10 +41,20 @@ const List = () => {
     getGames("first", "");
   }, []);
 
+  useEffect(() => {
+    // getGames("first", "");
+    if (dataFromChild !== "") {
+      getGames("search", dataFromChild);
+    } else {
+      getGames("first", "");
+    }
+  }, [dataFromChild]);
+
   return (
     <>
       <div className="w-full pb-5">
-        <div className="grid md:grid-cols-3 xs:grid-cols-1 sm:grid-cols-2 gap-6 ">
+        <Search setDataFromChild={setDataFromChild} />
+        <div className="grid md:grid-cols-3 xs:grid-cols-1 sm:grid-cols-2 gap-6 mt-4 ">
           {games.map((game) => (
             <React.Fragment key={game.name}>
               <GameCard game={game} />
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
-const Search = () => {
+const Search = ({ setDataFromChild }) => {
+
+  const handleSearchChange = (e) => {
+    setDataFromChild(e.target.value);
+  };
+
+
   return (
-    <div>Search</div>
-  )
-}
+    <div className="w-auto flex justify-center items-center">
+      <input
+        placeholder="Search"
+        className="p-2 w-2/5 mr-3 flex rounded"
+        onChange={handleSearchChange}
+      />
+    </div>
+  );
+};
 
-export default Search
+export default Search;
